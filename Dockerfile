5bf519b2f0e28d83d81f18c7d726b580efc52065
ddella <48737096+ddella@users.noreply.github.com>
2022-02-07 22:14:42 -0500

    Added pdo, pdo_mysql and mysqli

--- Dockerfile
+++ Dockerfile
@@ -10,7 +10,7 @@ RUN ["apk", "--no-cache", "upgrade"]
 RUN ["apk", "--no-cache", "add", "nginx"]
 
 # PHP8 Installation
-RUN ["apk", "--no-cache", "add", "php8", "php8-fpm"]
+RUN ["apk", "--no-cache", "add", "php8", "php8-fpm", "php8-pdo", "php8-pdo_mysql", "php8-mysqli"]
 
 COPY php8.sh /root
 # Make the script 'executable'. Prevents the error: permission denied unknown
@@ -18,7 +18,7 @@ RUN ["chmod", "+x", "/root/php8.sh"]
 RUN ["/root/php8.sh"]
 RUN ["rm", "-f", "/root/php8.sh"]
 
-# Creating a user and group 'www' for nginx 
+# Creating a user and group 'www' for nginx
 RUN ["adduser", "-D", "-H", "www", "www"]
 
 # Create the root directory for Nginx
@@ -37,8 +37,8 @@ COPY --chown=www:www nginx-certificate.crt /etc/nginx/certificate/nginx-certific
 COPY ["nginx.key", "/etc/nginx/certificate/nginx.key"]
 RUN ["chmod", "400", "/etc/nginx/certificate/nginx.key"]
 
-# Copy local html/php files & set user/group
-COPY --chown=www:www ./www/* /www/
+# Copy recursively local html/php files & set user/group
+COPY --chown=www:www ./www/ /www/
 
 # Expose port 80, 443
 EXPOSE 80
@@ -46,4 +46,4 @@ EXPOSE 443
 
 # Start PHP-fpm (FastCGI Process Manager) and Nginx
 COPY ["entrypoint.sh", "/root"]
-ENTRYPOINT ["sh", "/root/entrypoint.sh"] 
+ENTRYPOINT ["sh", "/root/entrypoint.sh"]
--- php8.sh
+++ php8.sh
@@ -18,7 +18,7 @@ sed -i "s|;listen.mode\s*=\s*0660|listen.mode = ${PHP_FPM_LISTEN_MODE}|g" /etc/p
 sed -i "s|user\s*=\s*nobody|user = ${PHP_FPM_USER}|g" /etc/php8/php-fpm.d/www.conf
 sed -i "s|group\s*=\s*nobody|group = ${PHP_FPM_GROUP}|g" /etc/php8/php-fpm.d/www.conf
 sed -i "s|;log_level\s*=\s*notice|log_level = notice|g" /etc/php8/php-fpm.d/www.conf #uncommenting line
-sed -i "s|;clear_env\s*=\s*no|clear_env = no|g" /etc/php8/php-fpm.d/www.conf #uncommenting line 
+sed -i "s|;clear_env\s*=\s*no|clear_env = no|g" /etc/php8/php-fpm.d/www.conf #uncommenting line
 
 sed -i "s|display_errors\s*=\s*Off|display_errors = ${PHP_DISPLAY_ERRORS}|i" /etc/php8/php.ini
 sed -i "s|display_startup_errors\s*=\s*Off|display_startup_errors = ${PHP_DISPLAY_STARTUP_ERRORS}|i" /etc/php8/php.ini
@@ -28,3 +28,5 @@ sed -i "s|;*upload_max_filesize =.*|upload_max_filesize = ${PHP_MAX_UPLOAD}|i" /
 sed -i "s|;*max_file_uploads =.*|max_file_uploads = ${PHP_MAX_FILE_UPLOAD}|i" /etc/php8/php.ini
 sed -i "s|;*post_max_size =.*|post_max_size = ${PHP_MAX_POST}|i" /etc/php8/php.ini
 sed -i "s|;*cgi.fix_pathinfo=.*|cgi.fix_pathinfo= ${PHP_CGI_FIX_PATHINFO}|i" /etc/php8/php.ini
+sed -i "s|;extension=mysqli.*|extension=mysqli|i" /etc/php8/php.ini
+sed -i "s|;extension=pdo_mysql.*|extension=pdo_mysql|i" /etc/php8/php.ini
